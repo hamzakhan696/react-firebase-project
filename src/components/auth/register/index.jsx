a702124e6394cd407ae2819984bffabaa3ddbf01
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-05-23 03:45:25 +0500

    fixes

--- src/components/auth/register/index.jsx
+++ src/components/auth/register/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Navigate, Link, useNavigate } from 'react-router-dom'
+import { Navigate, Link } from 'react-router-dom'
 import { useAuth } from '../../../contexts/authContext'
 import { doCreateUserWithEmailAndPassword } from '../../../firebase/auth'
 
--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -5,8 +5,6 @@ import {
   sendPasswordResetEmail,
   sendEmailVerification,
   updatePassword,
-  signInWithPopup,
-  GoogleAuthProvider,
 } from "firebase/auth";
 
 export const doCreateUserWithEmailAndPassword = async (email, password) => {
