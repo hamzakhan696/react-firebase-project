cadd599feec59f08c3123f675a1782f01b41b61a
hamzakhan286692 <hamzakhan286692@gmail.com>
2024-05-23 01:30:45 +0500

    authentication added successfully

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
-  };
+  apiKey: "AIzaSyB8K0879FLT2e5zzp7cqoQdmKeZEHiDVhE",
+  authDomain: "myfirebase-react-project-19a55.firebaseapp.com",
+  projectId: "myfirebase-react-project-19a55",
+  storageBucket: "myfirebase-react-project-19a55.appspot.com",
+  messagingSenderId: "394750304393",
+  appId: "1:394750304393:web:62b5d926c0032e67caa658",
+  measurementId: "G-SYDWWX6JMJ"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 
