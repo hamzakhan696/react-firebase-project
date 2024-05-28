87bdd67fcbafa1860ed3048467925be0274eae2d
SaqibSyed1014 <saqibsyed1014@gmail.com>
2024-05-28 16:16:31 +0500

    remove some extra classes

--- src/components/auth/login/index.jsx
+++ src/components/auth/login/index.jsx
@@ -107,7 +107,7 @@ const Login = () => {
                 required
                 value={email}
                 onChange={(e) => { setEmail(e.target.value) }}
-                className="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white outline-none"
+                className="bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none"
               />
             </div>
 
