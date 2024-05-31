d5d111cc2cead13b088195fb0b8f9f3a10819a03
SaqibSyed1014 <saqibsyed1014@gmail.com>
2024-05-31 17:27:52 +0500

    buttons label changed

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -136,14 +136,14 @@ const HomeComponent = () => {
       <div className="mx-2 md:mx-1 mt-20 flex">
       <button  className="text-nowrap w-full md:w-auto text-white bg-blue-600 py-2 px-4 rounded-md transition duration-300 ease-in-out hover:bg-blue-700">
         <Link to="/order-history" className="w-full inline-block text-center">
-          My Order History
+          My Orders
         </Link>
       </button>
       <button onClick={openAddToCart}  className="flex mx-2 text-nowrap w-full md:w-auto text-white bg-slate-900 py-2 px-4 rounded-md">
         <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
          <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
         </svg> 
-        Add to Cart
+        View Cart
       </button>
 </div>
         <div class="w-4/6	max-w-sm mx-auto mt-20">
