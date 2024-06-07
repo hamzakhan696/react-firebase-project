bf55b5c8d77b456cf60afd278c42a20c83f6f3d0
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-06-08 01:57:02 +0500

    product detail ui updated

--- src/components/product-detail/index.jsx
+++ src/components/product-detail/index.jsx
@@ -29,27 +29,29 @@ const ProductDetailComponent = () => {
   return (
     <div className="relative mx-5 mx-md-32 mt-5 mb-5">
       {/* Logo image behind the cards */}
-      <div className="absolute inset-0 flex justify-center items-center opacity-10 pointer-events-none z-0">
+      <div className="fixed inset-0 flex justify-center items-center opacity-10 pointer-events-none z-0">
         <img src="/logo.jpg" alt="logo" className="img-fluid h-full w-full" style={{ width: '50%', height: '50%', objectFit: 'contain' }} />
       </div>
 
       {/* Cards container */}
-      <div className="relative z-10 max-w-xs rounded-xl px-8 py-5 text-gray-600 shadow-2xl mt-12 bg-white">
+      <div className="bg-white relative max-w-xs rounded-xl overflow-hidden text-gray-600 border border-gray-300 shadow-2xl z-10 mt-14">
         {product ? (
           <React.Fragment key={product.id}>
-            <div className="mb-4 w-32 rounded-md bg-blue-100 px-3 py-1 text-sm font-medium text-blue-700">Product Details</div>
+            <div className="absolute top-4 left-4 mb-4 w-20 rounded-md bg-blue-100 px-4 py-1 text-sm font-medium text-blue-700 z-10">Product</div>
             {product.image && (
-              <div className="my-4">
-                <img src={product.image} alt={product.name} className="w-100 h-100 object-cover" />
-              </div>
+                <div className="w-full h-[250px] overflow-hidden">
+                  <img src={product.image} alt={product.name} className="w-full h-full object-cover hover:scale-150 transition duration-300" />
+                </div>
             )}
-            <div className="mb-2 text-2xl">{product.name}</div>
-            <div className="mb-6 text-gray-400">
-              <p><span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Price:</span> ${product.price}</p>
-              <p><span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Description:</span> {product.description}</p>
-              <p><span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Manufacturer:</span> {product.manufacturer}</p>
-              <p><span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Stock quantity:</span> {product.stock_quantity}</p>
-              <p><span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Type:</span> {product.type}</p>
+            <div className="px-8 py-5">
+              <div className="text-ellipsis font-bold mb-2 text-2xl z-10">{product.name}</div>
+              <div className="mb-6 text-gray-400">
+                <p><span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Price:</span> ${product.price}</p>
+                <p><span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Description:</span> {product.description}</p>
+                <p><span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Manufacturer:</span> {product.manufacturer}</p>
+                <p><span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Stock quantity:</span> {product.stock_quantity}</p>
+                <p><span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Type:</span> {product.type}</p>
+              </div>
             </div>
           </React.Fragment>
         ) : (
