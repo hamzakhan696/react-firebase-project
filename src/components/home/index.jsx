432d38b955dd3b16a9d7f705fc7b8b5512d9187c
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-06-08 05:33:19 +0500

    home page ui fixes

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -11,6 +11,7 @@ import { getAuth } from 'firebase/auth';
 const HomeComponent = () => {
   const [products, setProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [allProducts, setAllProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [cart, setCart] = useState([]);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -24,6 +25,7 @@ const HomeComponent = () => {
       const productsData = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
       setProducts(productsData);
       setFilteredProducts(productsData);
+      setAllProducts(productsData);
       setIsLoading(false);
     };
 
@@ -40,11 +42,6 @@ const HomeComponent = () => {
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(cart));
   }, [cart]);
-
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-    filterProducts(event.target.value);
-  };
 
   const filterProducts = (searchTerm) => {
     const filtered = products.filter(product =>
@@ -154,14 +151,34 @@ const HomeComponent = () => {
     }
   };
 
+  const [isDropdownOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isDropdownOpen);
+  };
+
+  const searchByParam = (filterParam) => {
+    let filtered = [];
+    if (filterParam === 'type') {
+      filtered = allProducts.filter(product => product.type.toLowerCase().includes(searchTerm.toLowerCase()));
+    } else if (filterParam === 'manufacturer') {
+      filtered = allProducts.filter(product => product.manufacturer.toLowerCase().includes(searchTerm.toLowerCase()));
+    } else if (filterParam === 'reset') {
+      filtered = allProducts;
+      setSearchTerm('');
+    }
+    setFilteredProducts(filtered);
+    setIsOpen(false);
+  }
+
   return (
     <div className="product-container relative pb-12">
     <div className="fixed inset-0 flex justify-center items-center opacity-10 pointer-events-none z-0">
-      <img src="/logo.jpg" alt="logo" className="img-fluid h-full w-full" style={{ width: '50%', height: '50%', objectFit: 'contain' }} />
+      <img src="/logo.jpg" alt="logo" className="img-fluid h-full w-full scale-110" style={{ width: '50%', height: '50%', objectFit: 'contain' }} />
     </div>
-    <h2 className="mt-16 title text-center z-10">Home Page</h2>
+    <h2 className="mt-16 title text-center text-xl z-10">Home Page</h2>
 
-    <div className="flex justify-center z-10">
+    <div className="flex justify-between z-10">
       <div className="mx-2 md:mx-1 mt-20 flex">
         <button className="text-nowrap w-full md:w-auto text-white bg-blue-600 py-2 px-4 rounded-md transition duration-300 ease-in-out hover:bg-blue-700 z-10">
           <Link to="/order-history" className="w-full inline-block text-center">
@@ -175,19 +192,50 @@ const HomeComponent = () => {
           View Cart
         </button>
       </div>
-      <div className="w-4/6 max-w-sm mx-auto mt-20 z-10">
-        <form className="relative">
+      <div className="flex items-center mt-20 z-10">
+        <div className="relative">
           <input
             type="text"
-            className="overflow-hidden whitespace-nowrap text-ellipsis w-full pl-10 lg:mx-44 pr-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
-            placeholder="Search by type and manufacturer..." value={searchTerm} onChange={handleSearchChange}
+            className="overflow-hidden whitespace-nowrap text-ellipsis pl-10 pr-4 py-2 border border-gray-300 rounded-l-lg shadow-sm focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+            placeholder="Search" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}
           />
-          <svg className="absolute lg:mx-44 left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <svg className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M12.9 14.32a8 8 0 111.414-1.414l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387zM14 8a6 6 0 11-12 0 6 6 0 0112 0z" clipRule="evenodd" />
           </svg>
-        </form>
+        </div>
+
+        <div className="relative inline-block text-left shrink-0">
+          <button onClick={toggleDropdown} className="border border-blue-500 bg-blue-500 text-white font-semibold py-2 px-4 rounded-r-md focus:outline-none focus:ring focus:ring-blue-200 flex items-center gap-2">
+            Filter
+            <svg className={`fill-current h-4 w-4 transform ${isDropdownOpen ? 'rotate-180' : ''}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M10 12l-5-5 1.41-1.41L10 9.17l3.59-3.58L15 7z"/>
+            </svg>
+          </button>
+
+          {isDropdownOpen && (
+              <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                  <div onClick={() => searchByParam('type')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 cursor-pointer" role="menuitem">
+                    By Type
+                  </div>
+                  <div onClick={() => searchByParam('manufacturer')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 cursor-pointer" role="menuitem">
+                    By Manufacturer
+                  </div>
+                  {searchTerm &&
+                      <div onClick={() => searchByParam('reset')}
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-red-400 hover:text-white cursor-pointer"
+                        role="menuitem">
+                        Reset
+                      </div>
+                  }
+                </div>
+              </div>
+          )}
+        </div>
       </div>
     </div>
+
+
     {isLoading && (
       <div className='spinner-overlay z-10'>
         <Oval
@@ -202,54 +250,67 @@ const HomeComponent = () => {
       </div>
     )}
 
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mx-md:16 mt-12 relative z-10 ">
-      {filteredProducts.map(product => (
-        <div key={product.id} className="bg-white relative max-w-xs rounded-xl overflow-hidden text-gray-600 border border-gray-300 shadow-2xl z-10">
-          <Link to={`/product/${product.id}`}>
-            <div className="absolute top-4 left-4 mb-4 w-20 rounded-md bg-blue-100 px-4 py-1 text-sm font-medium text-blue-700 z-10">Product</div>
-            {product.image && (
-                <div className="w-full h-[250px] overflow-hidden">
-                  <img src={product.image} alt={product.name} className="w-full h-full object-cover hover:scale-150 transition duration-300" />
-                </div>
-              )}
-            <div className="px-8 py-5">
-              <div className="text-ellipsis font-bold mb-2 text-2xl z-10">{product.name}</div>
-              <div className="mb-6 text-gray-400 z-10">
-                <p> <span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Price:</span> ${product.price}</p>
-                <p>  <span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Manufacturer:</span> {product.manufacturer}</p>
-                <p>  <span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Stock Quantity:</span> {product.stock_quantity}</p>
-                <p>  <span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Type:</span> {product.type}</p>
-              </div>
+      {
+        filteredProducts.length ?
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mx-md:16 mt-12">
+              {filteredProducts.map(product => (
+                  <div key={product.id} className="bg-white relative max-w-xs rounded-xl overflow-hidden text-gray-600 border border-gray-300 shadow-2xl z-10">
+                    <Link to={`/product/${product.id}`}>
+                      <div className="absolute top-4 left-4 mb-4 w-20 rounded-md bg-blue-100 px-4 py-1 text-sm font-medium text-blue-700 z-10">Product</div>
+                      {product.image && (
+                          <div className="w-full h-[250px] overflow-hidden">
+                            <img src={product.image} alt={product.name} className="w-full h-full object-cover hover:scale-150 transition duration-300" />
+                          </div>
+                      )}
+                      <div className="px-8 py-5 mb-5">
+                        <div className="text-ellipsis font-bold mb-2 text-2xl z-10">{product.name}</div>
+                        <div className="mb-6 text-gray-400 z-10">
+                          <p> <span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Price:</span> ${product.price}</p>
+                          <p>  <span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Manufacturer:</span> {product.manufacturer}</p>
+                          <p>  <span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Stock Quantity:</span> {product.stock_quantity}</p>
+                          <p>  <span className='text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis'>Type:</span> {product.type}</p>
+                        </div>
+                      </div>
+                    </Link>
+                    <div className="">
+                      {product.stock_quantity === 0 ? (
+                          <p className="text-red-500 font-bold z-10">Out of Stock</p>
+                      ) : (
+                          <button
+                              onClick={() => handleAddToCart(product)}
+                              className="flex items-center justify-center w-full absolute bottom-0 rounded-b-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300 z-10"
+                          >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                            </svg>
+                            Add to cart
+                          </button>
+                      )}
+                    </div>
+                  </div>
+              ))}
+             </div> :
+            <div className="flex justify-center items-center w-full h-[45vh]">
+              <p className="text-lg font-semibold">No products found</p>
             </div>
-          </Link>
-          <div className="">
-            {product.stock_quantity === 0 ? (
-              <p className="text-red-500 font-bold z-10">Out of Stock</p>
-            ) : (
-              <button
-                onClick={() => handleAddToCart(product)}
-                className="flex items-center justify-center w-full rounded-b-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300 z-10"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                </svg>
-                Add to cart
-              </button>
-            )}
-          </div>
-        </div>
-      ))}
-    </div>
+      }
+
 
     <div className={`fixed inset-y-0 right-0 w-80 bg-gray-100 overflow-y-scroll transform ${isSidebarOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out shadow-lg z-10`}>
-      <div className="flex justify-between items-center p-4 border-b border-gray-300 ">
-        <h3 className="text-xl font-bold mt-16">Cart</h3>
-        <button onClick={handleClearCart} className="self-end mt-20 ms-32 px-4 py-2 text-sm font-medium text-white bg-gray-600 rounded hover:bg-gray-700">Clear</button>
-        <button onClick={handleCloseSidebar} className="text-gray-600 hover:text-gray-800 ">
+      <div className="p-4 border-b border-gray-300 h-[150px] pt-14 relative text-end">
+        <button onClick={handleCloseSidebar} className="text-gray-600 hover:text-gray-800 pb-4">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
+        <div className="flex justify-between items-center">
+          <h3 className="text-xl font-bold">Cart</h3>
+          <button onClick={handleClearCart}
+                  className="self-end ms-32 px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded hover:bg-blue-700">
+            Clear
+          </button>
+        </div>
+
       </div>
 
       <div className="p-4">
@@ -261,8 +322,8 @@ const HomeComponent = () => {
               <div key={index} className="mb-4 p-2 border-b border-gray-300 ">
                 <div className="flex justify-between items-center">
                   <div>
-                  <div>
-                    <img src={item.image} alt={item.name} style={{ width: '60px' }} />
+                  <div className="mb-2">
+                    <img src={item.image} alt={item.name} style={{ width: '90px' }} />
                   </div>
                     <div className="text-lg font-semibold text-ellipsis">Name: {item.name}</div>
                     <div className="text-gray-500">Price: ${item.price}</div>
