cbce79fd64aaab8aa4e12c70c5957004c77489e1
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-06-09 11:49:26 +0500

    ui changes

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -296,8 +296,8 @@ const HomeComponent = () => {
       }
 
 
-    <div className={`fixed inset-y-0 right-0 w-80 bg-gray-100 overflow-y-scroll transform ${isSidebarOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out shadow-lg z-10`}>
-      <div className="p-4 border-b border-gray-300 h-[150px] pt-14 relative text-end">
+    <div className={`fixed z-30 inset-y-0 right-0 w-80 bg-gray-100 overflow-y-scroll transform ${isSidebarOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out shadow-lg z-10`}>
+      <div className="p-4 border-b border-gray-300 h-[120px] relative text-end">
         <button onClick={handleCloseSidebar} className="text-gray-600 hover:text-gray-800 pb-4">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
