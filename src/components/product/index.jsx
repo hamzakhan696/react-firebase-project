3d7d05a9533b498c7f831a7ae0fac81065520f03
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-06-09 12:45:31 +0500

    ui

--- src/components/product/index.jsx
+++ src/components/product/index.jsx
@@ -286,7 +286,7 @@ const ProductComponent = () => {
                     <span className="ml-2 text-sm text-gray-600">{product.type}</span>
                   </div>
                   </div>
-                  <button className="mt-4 flex items-center space-x-2 rounded-md border-2 border-blue-500 px-4 py-2 font-medium text-blue-600 transition hover:bg-blue-500 hover:text-white"><span>More Detail</span><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6"><path fillRule="evenodd" d="M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l-2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z" clipRule="evenodd"></path></svg></span></button>
+                  <button className="mt-4 w-full flex justify-center items-center space-x-2 rounded-b-xl border-2 border-blue-500 px-4 py-2 font-medium text-blue-600 transition hover:bg-blue-500 hover:text-white"><span>More Detail</span><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6"><path fillRule="evenodd" d="M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l-2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z" clipRule="evenodd"></path></svg></span></button>
                 </div>
               </Link>
             </div>
